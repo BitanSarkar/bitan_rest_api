cbf254818158e7e2df41da1ec324a16cfb9da40a
bitanSarkar <bitansarkar12345@gmail.com>
2021-12-01 11:03:15 +0530

    fix: cors origin

--- src/main/java/com/bitan/bitan/controllers/BitanControllers.java
+++ src/main/java/com/bitan/bitan/controllers/BitanControllers.java
@@ -1,6 +1,7 @@
 package com.bitan.bitan.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,17 +15,20 @@ public class BitanControllers {
 	
 	int ctr = 0;
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping(value = "/get")
 	public int count() {
 		return ctr;
 	}
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping(value = "/inc")
 	public int inc() {
 		ctr = service.inc(ctr);
 		return ctr;
 	}
 	
+	@CrossOrigin(origins = "*")
 	@GetMapping(value = "/dec")
 	public int dec() {
 		ctr = service.dec(ctr);
